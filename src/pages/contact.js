dd667fe1da784799699ecd01f7afd8fd4165888a
kylahmoon <kylahm@uw.edu>
2024-11-12 12:22:49 -0800

    Started adding dynamic entry for boxes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,9 +8,75 @@ function ContactUsPage() {
       <div style={{ marginTop: '10vh' }}>
         <SectionHeader title="Contact Us" subtitle="We'd like to talk to you too! We'll be in touch soon." />
       </div>
-      <div className="contact-box"></div>
+      <div className="contact-box">
+        <div className="box-left">
+          <p className="title">Let's Chat</p> 
+        </div>
+        <div className="box-right">
+          {/*image*/}
+        </div>
+      </div>
     </>
   );
 }
 
-export default ContactUsPage;
+export default ContactUsPage; 
+
+// export default ContactUsPage;
+
+
+// Attemting dynamic coding
+
+/*
+function ContactUsPage() {
+  const [formData, setFormData] = useState({
+    name: '',
+    subject: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value   
+ });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission here, e.g., send data to a server
+    console.log(formData);
+    // Clear   
+ form fields after submission
+    setFormData({
+      name: '',
+      subject: '',
+      email: '',
+      message: '',
+    });
+  };
+
+  return (
+    <>
+      <div style={{ marginTop: '10vh' }}>
+        <SectionHeader title="Contact Us" subtitle="We'd like to talk to you too! We'll be in touch soon." />
+      </div>
+      <div className="contact-box">
+        <div className="box-left">
+          <p className="title">Let's Chat</p>
+        </div>
+        <div className="box-right">
+          <form onSubmit={handleSubmit}>
+            <input type="text" id="name" name="name" placeholder="Name" value={formData.name} onChange={handleChange} />
+            <input type="text" id="subject" name="subject" placeholder="Subject" value={formData.subject} onChange={handleChange} />
+            <input type="email" id="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} />
+            <textarea id="message" name="message" placeholder="Message" value={formData.message} onChange={handleChange}/>
+            <button type="submit">Submit</button>
+            <button type="reset">ReThink</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ContactUsPage; */
