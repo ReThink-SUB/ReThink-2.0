0d94f1a4e86c5819f9cf3b850287de4a2ed0ba5c
kylahmoon <kylahm@uw.edu>
2024-11-11 19:01:42 -0800

    Added Css Page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import SectionHeader from '../components/SectionHeader/SectionHeader';
-
+import './contact.css';
 
 function ContactUsPage() {
-  const boxStyle = {
-    backgroundColor: '#E9F3E7',
-    padding: '20px',
-    margin: '10vh', 
-    height: `calc(120vh - 160px)`,
-    overflow: 'auto', 
-  };
-
   return (
     <>
       <div style={{ marginTop: '10vh' }}>
-      <SectionHeader title="Contact Us" subtitle="We'd like to talk to you too! We'll be in touch soon." />
-    </div>
-      <div style={boxStyle}><div/>
-    </div>
-  </>
+        <SectionHeader title="Contact Us" subtitle="We'd like to talk to you too! We'll be in touch soon." />
+      </div>
+      <div className="contact-box"></div>
+    </>
   );
 }
 
