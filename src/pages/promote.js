ef53621559895cb6921af122d13ed6ca1f5e6403
isha1218 <ishita.mundra@gmail.com>
2024-11-21 12:29:28 -0800

    fixed promote business page

--- src/pages/promote.js
+++ src/pages/promote.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../styles/promote.css';
-import RethinkLogoNoBackground from '../images/header/RethinkLogo-no-background.png'
-import RethinkPromoteBusinessImage from '../images/header/rethink-promote-business-image.png'
+import RethinkLogoNoBackground from '../images/RethinkLogo-no-background.png'
+import RethinkPromoteBusinessImage from '../images/rethink-promote-business-image.png'
 import '../components/SectionHeader/SectionHeader';
 import SectionHeader from '../components/SectionHeader/SectionHeader';
 
 function PromotePage() {
+  const FORM_URL = "https://forms.office.com/Pages/ResponsePage.aspx?id=W9229i_wGkSZoBYqxQYL0gWQVJmCk39CiTHrFRMC5TFURUU3Mlc2ODJYQVREWklUOFFSV05ITEowSS4u";
   return (
     <div className="promote-page-container">
       <SectionHeader title="Promote Your Business" subtitle="Partner with ReThink and reach a community like yours that's deciated to sustainability"/>
@@ -13,7 +14,7 @@ function PromotePage() {
         <div className="bottom-row">
           <div className="reason-col">
             <p className="join-heading">Why you should join!:</p>
-            <CreateReasonBox reasonNumber="1" reasonHeading="Build Trust &<br />Integrity" reasonDescription="Become certified in our community as a sustainable business!"/>
+            <CreateReasonBox reasonNumber="1" reasonHeading="Build Trust & Integrity" reasonDescription="Become certified in our community as a sustainable business!"/>
             <CreateReasonBox reasonNumber="2" reasonHeading="Marketing" reasonDescription="You'll be marketed to UW students and faculty, 40,000+ people, and on our social media and our platforms through our website and articles!"/>
             <CreateReasonBox reasonNumber="3" reasonHeading="Attract Student Talent" reasonDescription="Find deeply knowledgable students about green business practices."/>
           </div>
@@ -24,7 +25,7 @@ function PromotePage() {
             <p className="join-text">Want to join a community of businesses dedicated to sustainability, just like you? Continue below to sign up and help your neighbors ReThink consumerism.</p>
             <img src={RethinkPromoteBusinessImage} alt="ReThink Promote Business" className="rethink-promote-business-image"/>
             <div className="sign-up-button">
-            <button onClick={() => window.open("https://forms.office.com/Pages/ResponsePage.aspx?id=W9229i_wGkSZoBYqxQYL0gWQVJmCk39CiTHrFRMC5TFURUU3Mlc2ODJYQVREWklUOFFSV05ITEowSS4u", "_blank")}>Sign Up</button>
+            <button onClick={() => window.open(FORM_URL, "_blank")}>Sign Up</button>
             </div>
           </div>
         </div>
@@ -41,7 +42,7 @@ function CreateReasonBox(props) {
           <p className="reason-box-number">{props.reasonNumber}</p>
         </div>
         <div className="reason-box-column">
-          <p className="reason-box-heading" dangerouslySetInnerHTML={{ __html: props.reasonHeading }}/>
+          <p className="reason-box-heading">{props.reasonHeading}</p>
           <p className="reason-box-description">{props.reasonDescription}</p>
         </div>
       </div>
