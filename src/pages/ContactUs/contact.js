acd2c496a93806f652d2bd1dd2ba2522ad441c4c
kylahmoon <kylahm@uw.edu>
2024-12-04 12:23:54 -0800

    Merge Changes

--- src/pages/ContactUs/contact.js
+++ src/pages/ContactUs/contact.js
@@ -13,16 +13,16 @@ function ContactUsPage() {
           <p className="title">Let's Chat!</p>
           <form>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" placeholder="Enter your name" />
+            <input type="text" id="name" name="name" placeholder="Enter your name:" />
             
             <label htmlFor="subject">Subject</label>
-            <input type="text" id="subject" name="subject" placeholder="Enter the subject" />
+            <input type="text" id="subject" name="subject" placeholder="Enter the subject:" />
             
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email" />
+            <input type="email" id="email" name="email" placeholder="Enter your email:" />
             
             <label htmlFor="message">Message</label>
-            <textarea id="message" name="message" placeholder="Write your message here"></textarea>
+            <textarea id="message" name="message" placeholder="Write your message here:"></textarea>
             
             <button type="submit">Submit</button>
           </form>
