dd3a4e9396f7690581aa1dacee6595bc3ecfda63
owenwong <owenwong@uw.edu>
2025-05-19 11:50:21 -0700

    Implemented emailjs functionality to contactus page

--- src/pages/ContactUs/contact.js
+++ src/pages/ContactUs/contact.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import SectionHeader from '../../components/SectionHeader/SectionHeader';
 import './style.css';
-// import emailjs from '@emailjs/browser';
+import emailjs from '@emailjs/browser';
 
 function ContactUsPage() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+    const serviceId = 'service_pazoz54';
+    const templateId = 'template_f9r6pje';
+    const publicKey = 'NWwowGG_uHhk0RZAZ';
+
+    emailjs.sendForm(serviceId, templateId, form.current, {
+      publicKey: publicKey,
+    })
+      .then((result) => {
+        console.log('Message sent successfully', result.text);
+        alert('Message sent successfully!');
+        form.current.reset();
+      }, (error) => {
+        console.log('Message failed to send', error.text);
+        alert('Failed to send message. Please try again.');
+      });
+  };
+
   return (
     <>
       <div className="header">
@@ -12,18 +34,18 @@ function ContactUsPage() {
       <div className="contact-box">
         <div className="box-left">
           <p className="title">Let's Chat!</p>
-          <form>
+          <form ref={form} onSubmit={sendEmail}>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" placeholder="Enter your name:" />
+            <input type="text" id="name" name="name" placeholder="Enter your name:" required />
             
             <label htmlFor="subject">Subject</label>
-            <input type="text" id="subject" name="subject" placeholder="Enter the subject:" />
+            <input type="text" id="subject" name="subject" placeholder="Enter the subject:" required />
             
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email:" />
+            <input type="email" id="email" name="email" placeholder="Enter your email:" required />
             
             <label htmlFor="message">Message</label>
-            <textarea id="message" name="message" placeholder="Write your message here:"></textarea>
+            <textarea id="message" name="message" placeholder="Write your message here:" required></textarea>
             
             <button type="submit">Submit</button>
           </form>
